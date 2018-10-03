eaaf88ac1232296156b8df7bb7910e4b10b1c5b4
FalconRay <falconray@yahoo.com>
2018-10-03 22:28:25 +0800

    fixed permission issue in case of building with vagrant.

---
+++ devenv/setupPost.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -o nounset
+set -o errexit
+
+set -e
+set -x
+
+# Install docker
+sudo usermod -a -G docker $USER # Add ubuntu user to the docker group
+
+# Set Go environment variables needed by other scripts
+export GOPATH="/opt/gopath"
+export GOROOT="/opt/go"
+PATH=$GOROOT/bin:$GOPATH/bin:$PATH
+
+sudo chown -R $USER:$USER $GOROOT
+
+# Create directory for the DB
+sudo mkdir -p /var/hyperledger
+sudo chown -R $USER:$USER /var/hyperledger
+
+# Ensure permissions are set for GOPATH
+sudo chown -R $USER:$USER $GOPATH
+
